26f5581e4401634e8406e551eec5c3edd5146b8f
Ben Moore <1326052+BenBMoore@users.noreply.github.com>
2023-05-14 17:08:25 +0100

    Initialise .minigit directory, and logging

--- minigit/cli.py
+++ minigit/cli.py
@@ -1,2 +1,27 @@
+import argparse
+import logging
+import os
+from . import data
+
+
 def main ():
-    print ('Hello, World!')
+    logging.basicConfig(level=logging.DEBUG)
+    args = parse_args ()
+    args.func (args)
+
+
+def parse_args ():
+    parser = argparse.ArgumentParser ()
+
+    commands = parser.add_subparsers (dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser ('init')
+    init_parser.set_defaults (func=init)
+
+    return parser.parse_args ()
+
+
+def init (args):
+    data.init()
+    logging.debug(f'Initialized data directory in {os.getcwd()}/{data.GIT_DIR}')
---
+++ minigit/data.py
@@ -0,0 +1,8 @@
+import os
+import logging
+
+GIT_DIR = '.minigit'
+
+def init():
+    os.mkdir(GIT_DIR)
+    logging.debug("Created .minigit directory")
